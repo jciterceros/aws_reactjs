5d730db40e43e9a8218686163f6d800845a70d4f
jciterceros <jciterceros@yahoo.com>
2022-08-19 23:05:04 -0300

    add buttons

--- src/App.js
+++ src/App.js
@@ -6,18 +6,33 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>Hello world</p>
             </header>
+            <div>
+                create calculator here
+                <div>
+                    <input type="text" />
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>0</button>
+                    <button>+</button>
+                    <button>-</button>
+                    <button>*</button>
+                    <button>/</button>
+                    <button>=</button>
+                </div>
+                <div>
+                    <input type="text" />
+                    <button>=</button>
+                </div>
+            </div>
         </div>
     );
 }
